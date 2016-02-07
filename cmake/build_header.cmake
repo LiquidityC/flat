df93bc04f16ad7d06f2b0d5c370ad4bb7faf910b
Linus Probert <linus.probert@gmail.com>
2016-02-07 19:00:15 +0100

    Fixed makefile generation

---
+++ cmake/build_header.cmake
@@ -0,0 +1,13 @@
+macro(build_header)
+	set(GLOB_DIR "${DIR_ROOT}/src/")
+	file(REMOVE ${DIR_ROOT}/src/flat.h)
+	file(GLOB headers RELATIVE ${GLOB_DIR} ${GLOB_DIR}/*.h)
+	file(WRITE ${DIR_ROOT}/src/flat.h "#ifndef FLAT_H_\n")
+	file(APPEND ${DIR_ROOT}/src/flat.h "#define FLAT_H_\n")
+	foreach(header ${headers})
+		file(APPEND ${DIR_ROOT}/src/flat.h "#include <${header}>\n")
+	endforeach()
+	file(APPEND ${DIR_ROOT}/src/flat.h "#endif\n")
+endmacro(build_header)
+
+build_header()
