3746e669bf6d281e8ad540ad28c5a437a7db6ade
Linus Probert <linus.probert@gmail.com>
2016-11-30 22:58:07 +0100

    Added a test and coveralls integration

---
+++ testsrc/AnimationTest.cpp
@@ -0,0 +1,30 @@
+#include "catch.hpp"
+#include "../src/Animation.h"
+
+TEST_CASE( "AnimationTest", "[animation]" )
+{
+	flat2d::Clips clips = {
+		{ 0, 0, 5, 5 },
+		{ 5, 0, 5, 5 }
+	};
+
+	flat2d::Animation animation(clips, 10);
+
+	SECTION( "StartStopTest", "[animation]" )
+	{
+		REQUIRE( !animation.isRunning() );
+		animation.start();
+		REQUIRE( animation.isRunning() );
+		animation.stop();
+		REQUIRE( !animation.isRunning() );
+	}
+
+	SECTION( "RunTest", "[animation]" )
+	{
+		const SDL_Rect *clip = animation.run();
+		REQUIRE(clip->x == 0);
+		REQUIRE(clip->y == 0);
+		REQUIRE(clip->w == 5);
+		REQUIRE(clip->h == 5);
+	}
+}
