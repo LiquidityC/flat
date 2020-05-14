d6e249dea5ab9be425e208e13ddc85398da3f3b8
Hugh Hoang <hugh.hoang@gmail.com>
2020-05-14 20:46:45 +1200

    chore: fix tab spacing

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -3,61 +3,61 @@
 
 namespace flat2d
 {
-  void Animation::start()
-  {
-    animationTimer.start();
-  }
-
-  void Animation::stop()
-  {
-    animationTimer.stop();
-  }
-
-  bool Animation::isRunning() const
-  {
-    return animationTimer.isStarted();
-  }
-
-  const SDL_Rect *Animation::run()
-  {
-    assert(!clips.empty());
-
-    if (!animationTimer.isStarted())
+    void Animation::start()
     {
-      return &clips[0];
+        animationTimer.start();
     }
 
-    // Stay at the last clip, if runs once
-    if (runOnce && clipIndex == clips.size())
+    void Animation::stop()
     {
-      animationTimer.stop();
-      return &clips[clipIndex];
+        animationTimer.stop();
     }
 
-    if (animationTimer.getTicks() >= timestep)
+    bool Animation::isRunning() const
     {
-      clipIndex++;
-      animationTimer.stop();
-      animationTimer.start();
+        return animationTimer.isStarted();
     }
 
-    clipIndex = clipIndex % clips.size();
-
-    return &clips[clipIndex];
-  }
-
-  void Animation::setRunOnce(bool once)
-  {
-    runOnce = once;
-  }
-
-  bool Animation::reset(bool alsoStart)
-  {
-    clipIndex = 0;
-    animationTimer.stop();
-    if (alsoStart)
+    const SDL_Rect *Animation::run()
     {
-      animationTimer.start();
+        assert(!clips.empty());
+
+        if (!animationTimer.isStarted())
+        {
+            return &clips[0];
+        }
+
+        // Stay at the last clip, if runs once
+        if (runOnce && clipIndex == clips.size())
+        {
+            animationTimer.stop();
+            return &clips[clipIndex];
+        }
+
+        if (animationTimer.getTicks() >= timestep)
+        {
+            clipIndex++;
+            animationTimer.stop();
+            animationTimer.start();
+        }
+
+        clipIndex = clipIndex % clips.size();
+
+        return &clips[clipIndex];
     }
-  }
+
+    void Animation::setRunOnce(bool once)
+    {
+        runOnce = once;
+    }
+
+    bool Animation::reset(bool alsoStart)
+    {
+        clipIndex = 0;
+        animationTimer.stop();
+        if (alsoStart)
+        {
+            animationTimer.start();
+        }
+    }
 } // namespace flat2d
